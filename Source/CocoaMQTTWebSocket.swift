1e4236198124f446c487cef02f9fc3ad78a4bdda
cui.chang <254061632@qq.com>
2022-03-29 14:14:30 +0800

    let newWrite = try? WriteItem(tag: tag, timeout: (timeout > 0.0) ? .now() + timeout : .distantFuture)

--- Source/CocoaMQTTWebSocket.swift
+++ Source/CocoaMQTTWebSocket.swift
@@ -131,7 +131,7 @@ public class CocoaMQTTWebSocket: CocoaMQTTSocketProtocol {
     
     public func write(_ data: Data, withTimeout timeout: TimeInterval, tag: Int) {
         internalQueue.async (flags: .barrier){
-                let newWrite = WriteItem(tag: tag, timeout: (timeout > 0.0) ? .now() + timeout : .distantFuture)
+                let newWrite = try? WriteItem(tag: tag, timeout: (timeout > 0.0) ? .now() + timeout : .distantFuture)
                if(newWrite.tag <= self.scheduledWrites.count){
                    self.scheduledWrites.insert(newWrite)
                }
